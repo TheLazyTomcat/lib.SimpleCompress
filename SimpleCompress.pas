a89aa58ca1df5bc4e5dd54ebec3f2456d39a66a2
František Milt <frantisek.milt@gmail.com>
2015-01-10 16:07:20 +0100

    Added basic FPC support

--- SimpleCompress.pas
+++ SimpleCompress.pas
@@ -2,9 +2,9 @@
 
 Simple (De)Compression routines (powered by ZLib)
 
-©František Milt 4.2.2012
+©František Milt 2015-01-10
 
-Version 1.0
+Version 1.1
 
 ===============================================================================}
 unit SimpleCompress;
@@ -14,21 +14,21 @@ interface
 uses
   Classes;
 
-Function ZCompressStream(aStream: TStream): Boolean;
+Function ZCompressStream(Stream: TStream): Boolean;
 Function ZCompressFile(const FileName: String): Boolean;
 
-Function ZDecompressStream(aStream: TStream): Boolean;
+Function ZDecompressStream(Stream: TStream): Boolean;
 Function ZDecompressFile(const FileName: String): Boolean;
 
 implementation
 
 uses
-  SysUtils, Zlib;
+  SysUtils, {$IFDEF FPC}ZStream{$ELSE}Zlib{$ENDIF};
 
 const
   buffer_size = 4096{Bytes};
 
-Function ZCompressStream(aStream: TStream): Boolean;
+Function ZCompressStream(Stream: TStream): Boolean;
 var
   TempStream:     TMemoryStream;
   CompressionStr: TCompressionStream;
@@ -38,12 +38,12 @@ try
   try
     CompressionStr := TCompressionStream.Create(clDefault,TempStream);
     try
-      CompressionStr.CopyFrom(aStream,0);
+      CompressionStr.CopyFrom(Stream,0);
     finally
       CompressionStr.Free;
     end;
-    aStream.Position := 0;
-    aStream.Size := aStream.CopyFrom(TempStream,0);
+    Stream.Position := 0;
+    Stream.Size := Stream.CopyFrom(TempStream,0);
     Result := True;
   finally
     TempStream.Free;
@@ -52,6 +52,8 @@ except
   Result := False;
 end;
 end;
+
+//------------------------------------------------------------------------------
 
 Function ZCompressFile(const FileName: String): Boolean;
 var
@@ -69,28 +71,35 @@ except
 end;
 end;
 
-Function ZDecompressStream(aStream: TStream): Boolean;
+//------------------------------------------------------------------------------
+
+Function ZDecompressStream(Stream: TStream): Boolean;
 var
   TempStream:       TMemoryStream;
   DecompressionStr: TDecompressionStream;
-  BuffLen:          Integer;
-  Buffer:           Array[0..buffer_size - 1] of Byte;
+  BytesRead:        Integer;
+  Buffer:           Pointer;
 begin
 try
   TempStream := TMemoryStream.Create;
   try
-    aStream.Position := 0;
-    DecompressionStr := TDecompressionStream.Create(aStream);
+    Stream.Position := 0;
+    DecompressionStr := TDecompressionStream.Create(Stream);
     try
-      Repeat
-        BuffLen := DecompressionStr.Read(Buffer,SizeOf(Buffer));
-        TempStream.WriteBuffer(Buffer,BuffLen);
-      Until BuffLen <= 0;
+      GetMem(Buffer,buffer_size);
+      try
+        repeat
+          BytesRead := DecompressionStr.Read(Buffer^,SizeOf(Buffer));
+          TempStream.WriteBuffer(Buffer^,BytesRead);
+        until BytesRead <= 0;
+      finally
+        FreeMem(Buffer,buffer_size);
+      end;
     finally
       DecompressionStr.Free;
     end;
-    aStream.Position := 0;
-    aStream.Size := aStream.CopyFrom(TempStream,0);
+    Stream.Position := 0;
+    Stream.Size := Stream.CopyFrom(TempStream,0);
     Result := True;
   finally
     TempStream.Free;
@@ -99,6 +108,8 @@ except
   Result := False;
 end;
 end;
+
+//------------------------------------------------------------------------------
 
 Function ZDecompressFile(const FileName: String): Boolean;
 var
